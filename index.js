7856b76d0bd24e699955b8b68d9d3868c153733c
Sh3lby007 <zhengshaobin@ymail.com>
2022-10-17 23:56:08 +0800

    Delete previous code and start afresh.

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-const random_quote_api = 'http://api.quotable.io/random'
-const quoteDisplayElement = document.getElementById('quoteDisplay')
-const quoteInputElement = document.getElementById('quoteInput')
+// To create a typing speed game, what do I need? 
+/*
+1. A big box that contains random generated texts
+2. A highlighted bar that follows the text ater you have typed the word
+3. There probably should be an eventlistener to listen to input for the highlighted bar
+4. The highlighted bar needs highlight the errors made if any alphabet is typed incorrectly
+5. The timer will start counting down from 60 by itself when you start typing
+6. After the 
+7. The bar allows you to go back and edit on your mistakes.
+8. Once everything is done, it will display WPM, Characters typed, accuracy and a try again button which will reset everything
+*/
 
-console.log(random_quote_api)
 
-function getRandomQuote() {
-    return fetch(random_quote_api)
-        .then(response => response.json())
-        .then(data => data.content)
-}
+// How to calculate WPM = Total key pressed / time elasped in minute
 
-async function getNextQuote() {
-    const quote = await getRandomQuote()
-    quoteDisplayElement.textContent = quote
-    quoteInputElement.textContent = null
-    console.log(quote)
-}
+// The total key pressed will need to include spaces, numbers, letters and punctuation
 
+// How to calculate accuracy = Correct keys pressed / Total keys pressed
