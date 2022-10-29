9cf4d4e22e152b49bad41b3701dbe9d2d7832f66
Sh3lby007 <zhengshaobin@ymail.com>
2022-10-29 15:42:40 +0800

    Show live counter:     1. Live counter of wpm, mistakes and characters typed are enabled.     2. Remove unnecessary comments     3. Add comments for randomQuote function and variables in the event     listener.

--- index.js
+++ index.js
@@ -21,13 +21,11 @@ Step 4
 When the timer stops and the game ends, there needs to be a a compilation of WPM, how many characters are typed and the accuracy of the typing. A try again button also need to appear which resets all value to default and we can create a function call restart() to handle this.
 A new quote also needs to appear when we click the try again button, we can call a newQuote() function to handle this request.
 */
+
 /*
 Step 1:
 Assign variables to elements from the HTML document so that they can easily accessed and modified. Make sure that these variables are used multiple times as it is not good practice to declare global variables if you only gonna use them once
 */
-// Declare paragraph array and put in one paragraoh first to get stuff working, will include the api mehtod after this is working.
-const paragraphs = ["He ordered his regular breakfast. Two eggs sunnyside up, hash browns, and two strips of bacon. He continued to look at the menu wondering if this would be the day he added something new. This was also part of the routine. A few seconds of hesitation to see if something else would be added to the order before demuring and saying that would be all. It was the same exact meal that he had ordered every day for the past two years."]
-
 
 const inputField = document.getElementById('quoteInput')
 const quoteDisplayField = document.getElementById('quoteDisplay')
@@ -36,19 +34,21 @@ const mistakeTag = document.getElementById('mistake')
 const charTyped = document.getElementById('char-typed')
 const wpmTag = document.getElementById('wpm')
 const restartGame = document.getElementById('button')
-// The total key pressed will need to include spaces, numbers, letters and punctuation
-const totalKeys = paragraphs[0].split('').length
+
 let timeLeft = 60
 let charIndex = 0
+let wpmCharIndex = 0
 let mistakes = charIndex
 let isTyping = charIndex
 let timer
 
-
 inputField.addEventListener('input', () => {
-    const characters = quoteDisplayField.querySelectorAll('span')
-    let quoteArray = Object.keys(characters)
+    // This variable holds all the nodes of individual characters that are held in the span tag
+    let characters = quoteDisplayField.querySelectorAll('span')
+
+    // This variable holds the value of each characters user has typed
     let typedCharacter = inputField.value.split('')[charIndex]
+
     // If the there are still time left, we want the code below to continue to operate.
     if (timeLeft > 0) {
         // Once we call the countdown function when there is an input, it won't restart again on every key clicked.
@@ -66,6 +66,7 @@ inputField.addEventListener('input', () => {
         // If user hasn't entered any characters or press the backspace: 1. Decrease the character index count. 2. if the span contains incorrect class, remove it and reduce the mistake count. 3. Reduce both incorrect and correct classlist if backspace is clicked. 
         if (typedCharacter == null) {
             charIndex--
+            wpmCharIndex--
             if (characters[charIndex].classList.contains('incorrect')) {
                 mistakes--
             }
@@ -79,32 +80,35 @@ inputField.addEventListener('input', () => {
                 mistakes++
                 characters[charIndex].classList.add('incorrect')
             }
+            // Increase the character index number whenever user types in an input, dont care correct or incorrect character
             charIndex++
+            wpmCharIndex++
         }
-
+        // If the amount of characters type is equals to the length of the quote, we want to call a new quote and reset char index.
         if (characters.length === charIndex) {
 
-            await renderQuote()
+            renderQuote()
 
             // Reset char index, so that on new quote you can split and retrieve char again
             // But this will not affect the wpmCharIndex used for WPM calculation
             charIndex = 0
-
-            // Reset the input array once the new quote is available
-            inputArray = []
         }
-        // if (characters.length == charIndex) renderQuote()
-        // Increase the character index number whenever user types in an input, dont care correct or incorrect character
-    } else { // If the time becomes 0, we dont want user to be able to input anything in the field because we need to stop counting anything mistakes/wpm/characters typed. Only display the characters typed, mistakes made and wpm after timer stops.
-        inputField.value = ''
-        charTyped.innerText = 'Characters Typed:' + charIndex
+        // Only display the characters typed, mistakes made and wpm after timer stops.
+        charTyped.innerText = 'Characters Typed:' + wpmCharIndex
         mistakeTag.innerText = 'Mistakes:' + mistakes
         // How to calculate WPM = Total key pressed correctly divided by 5. Giving it a math.round function to round wpm up to the nearest integer because wpm can't have decimals.
-        let wpm = Math.round((charIndex - mistakes) / 5)
+        let wpm = Math.round((wpmCharIndex - mistakes) / 5)
         wpmTag.innerText = 'WPM:' + wpm
+
+    } else {
+        // If the time becomes 0, we dont want user to be able to input anything in the field because we need to stop counting anything mistakes/wpm characters typed. 
+        inputField.value = ''
     }
 })
 
+/**
+ * This functions fetches the random quote from the API
+ */
 function randomQuote() {
     return fetch('http://api.quotable.io/random')
         .then(response => response.json())
@@ -130,21 +134,10 @@ async function renderQuote() {
     // Clears the user input field
     inputField.value = null
 }
-renderQuote()
-// function displayParagraph() {
-//     // Spllittng all characters in the parapgrah and reiterate through the array and add span tag to each individual characters. 
-//     // This is so we can check manipulate each characters easily from now on and later manipulate them in the css file so they display the colour we want if the user enters wrong characters.
-//     paragraphs[0].split('').forEach(span => {
-//         let characterSpan = `<span>${span}</span>`;
-//         quoteDisplayField.innerHTML += characterSpan
-//         inputField.value = null
-//     })
-// }
-
-// displayParagraph()
-
 
 // Reloads the page and restarts the game
 restartGame.addEventListener('click', () => {
     location.reload()
 })
+
+renderQuote()
